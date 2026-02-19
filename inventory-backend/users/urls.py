89cd1eff6c370cff2b0eb8cd4f4c8d61e3fb3490
Harshi-code760 <harshavi.vegad@ada.ac.uk>
2026-02-19 13:02:38 +0000

    Trying to get JWT Token to work

--- inventory-backend/users/urls.py
+++ inventory-backend/users/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import RegisterView
+from .views import RegisterView, Profile
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
+    path('profile/', Profile.as_view(), name='profile'),
 ]
