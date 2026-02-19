dcb69eb88a6fb83cb5184796c18ed1d78275b924
Harshi-code760 <harshavi.vegad@ada.ac.uk>
2026-02-19 12:37:17 +0000

    Token Refresh and Access Worked

--- inventory-backend/config/urls.py
+++ inventory-backend/config/urls.py
@@ -24,6 +24,6 @@ from rest_framework_simplejwt.views import (
 
 urlpatterns = [
     path('api/auth/',include('users.urls')),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/auth/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/auth/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
